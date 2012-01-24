44f2f7ee2923704890a14ce8db5489ae676f7309
Jerry Odenwelder <jerry.odenwelder@versionone.com>
2012-01-24 12:45:25 -0500

    unzip now works again and I added checks to make sure files were available

--- rakefile.rb
+++ rakefile.rb
@@ -39,8 +39,7 @@ task :default => :build
 ###############################################################################
 desc "Builds the solution (default task)."
 
-task :build => [:build_v1, :build_dmanalytics, :run_datamart]
-
+task :build => [:build_v1, :build_dmanalytics, :run_datamart] 
 
 task :build_v1 => [:unzip_v1, :restore_v1, :upgrade_v1]
 task :build_dmanalytics => [:unzip_analytics, :restore_analytics, :upgrade_datamart, :upgrade_analytics]
@@ -69,24 +68,18 @@ end
 ###############################################################################
 desc "Unzip the production backup zipfile"
 task :unzip_v1 do
-	zip_filename = @prod_backup_filename_prefix + '.zip'
+	zip_filename = @prod_backup_filename_prefix + '.zip'	
 	zip_file = File.join(@zip_file_directory, zip_filename)
-	puts "Extract V1Prod backup from " + zip_filename
-	unzip do |zip|
-		zip.unzip_path = File.join File.dirname(__FILE__), @backup_file_directory
-		zip.zip_file = zip_file
-	end
+	fail "VersionOne zip file is not available." if not File.exist?(zip_file)
+	unzip_file(zip_file)
 end
 
 desc "Unzip the production analytics backup zipfile"
 task :unzip_analytics do
-	zip_filename = @analytics_backup_filename_prefix + '.zip'
+	zip_filename = @analytics_backup_filename_prefix + '.zip'	
 	zip_file = File.join(@zip_file_directory, zip_filename)
-	puts "Extract Analytics backup from " + zip_filename
-	unzip do |zip|
-		zip.unzip_path = File.join File.dirname(__FILE__), @backup_file_directory
-		zip.zip_file = zip_file
-	end
+	fail "Analytics zip file is not available." if not File.exist?(zip_file)
+	unzip_file(zip_file)	
 end
 
 ###############################################################################
@@ -94,8 +87,8 @@ end
 ###############################################################################
 desc "Restore the sandbox database from the produciton backup"
 task :restore_v1 do
-	backup_file = (Dir[File.join(File.expand_path(@backup_file_directory), @prod_backup_filename_prefix + '*.bak')]).to_s
-
+	backup_file = (Dir[File.join(File.expand_path(@backup_file_directory), @prod_backup_filename_prefix + '_*.bak')]).to_s	
+	fail "No VersionOne database file to restore" if not File.exist?(backup_file)
 	puts "Restore production backup file " + backup_file
 	cmd = %{
 		USE master
@@ -111,17 +104,16 @@ task :restore_v1 do
 		END
 		RESTORE DATABASE @dbname FROM DISK = '#{backup_file}' WITH FILE = 1, NOUNLOAD, REPLACE, STATS = 10, MOVE 'V1Production' TO '#{@database_dir}\\#{@v1_db_name}.mdf', MOVE 'V1Production_1' TO '#{@database_dir}\\#{@v1_db_name}.ldf', MOVE 'ftrow_V1Production' TO '#{@database_dir}\\#{@v1_db_name}.ndf'
 	}
-
     f = File.open(@prod_sql_restore_commandfile, "w")
     f.write(cmd)
-    f.close
+    f.close    
     sh "sqlcmd -S #{@sqlserver_name} -i " + @prod_sql_restore_commandfile, :verbose => true
 end
 
 desc "Restore the analytics database from the backup file"
 task :restore_analytics do
 	backup_file = (Dir[File.join(File.expand_path(@backup_file_directory), @analytics_backup_filename_prefix + '*.bak')]).to_s
-
+	fail "No Analytics database file to restore" if not File.exist?(backup_file)
 	puts "Restore analytics backup file " + backup_file
 	cmd = %{
 		USE master
@@ -137,10 +129,9 @@ task :restore_analytics do
 		END
 		RESTORE DATABASE @dbname FROM DISK = '#{backup_file}' WITH FILE = 1, NOUNLOAD, REPLACE, STATS = 10, MOVE 'V1Production-analytics' TO '#{@database_dir}\\#{@analytics_db_name}.mdf', MOVE 'V1Production-analytics_log' TO '#{@database_dir}\\#{@analytics_db_name}.ldf'
 	}
-
     f = File.open(@analytics_sql_restore_commandfile, "w")
     f.write(cmd)
-    f.close
+    f.close    
     sh "sqlcmd -S #{@sqlserver_name} -i " + @analytics_sql_restore_commandfile, :verbose => true
 end
 
@@ -149,14 +140,14 @@ end
 ###############################################################################
 desc "Upgrade Ultimate"
 task :upgrade_v1 do
-	setup_exe = (Dir[FileList["#{@installer_dir}/VersionOne.Setup-Ultimate*.exe"].last]).to_s
+	setup_exe = (Dir[FileList["#{@installer_dir}/VersionOne.Setup-Ultimate*.exe"].last]).to_s	
 	puts "Upgrading Sandbox using " + setup_exe
 	sh "#{setup_exe} http://localhost/#{@v1_instance_name} -quiet -r -AnalyticsUrl:http://prod01/#{@analytics_instance_name} -AnalyticsSigningKey:versionone"
 end
 
 desc "Upgrade Datamart"
 task :upgrade_datamart do
-	setup_exe = (Dir[FileList["#{@installer_dir}/Setup-VersionOne.DatamartLoader*.exe"].last]).to_s
+	setup_exe = (Dir[FileList["#{@installer_dir}/Setup-VersionOne.DatamartLoader*.exe"].last]).to_s	
 	puts "Upgrading Datamart using " + setup_exe
 	sh "#{setup_exe} \"#{@datamart_instance_name}\" /Action:Upgrade /Quiet:True"
 end
@@ -173,7 +164,7 @@ end
 ###############################################################################
 desc "Execute the Datamart Loader"
 task :run_datamart do
-	setup_exe = 'VersionOne.DatamartLoader.exe'
+	setup_exe = 'VersionOne.DatamartLoader.exe'	
 	Dir.chdir @dataloader_dir
 	sh "#{setup_exe}"
 end
@@ -183,13 +174,13 @@ end
 # #############################################################################
 task :remove_datamart do
 	print "Uninstall \"#{@datamart_instance_name}\"\n"
-	setup_exe = (Dir[FileList["#{@installer_dir}/Setup-VersionOne.DatamartLoader*.exe"].last]).to_s
+	setup_exe = (Dir[FileList["#{@installer_dir}/Setup-VersionOne.DatamartLoader*.exe"].last]).to_s	
 	begin
 		sh "#{setup_exe} \"#{@datamart_instance_name}\" /Action:Uninstall /Quiet:True /DeleteDatabase:True"
 	rescue
 		puts "NOTHING TO UNINSTALL!"
 	end
-
+	
 end
 
 task :remove_analytics do
@@ -205,8 +196,8 @@ end
 # Install Task
 # #############################################################################
 task :install_datamart do
-	print "Uninstall \"#{@datamart_instance_name}\"\n"
-	setup_exe = (Dir[FileList["#{@installer_dir}/Setup-VersionOne.DatamartLoader*.exe"].last]).to_s
+	print "Install \"#{@datamart_instance_name}\"\n"
+	setup_exe = (Dir[FileList["#{@installer_dir}/Setup-VersionOne.DatamartLoader*.exe"].last]).to_s	
 	sh "#{setup_exe} \"#{@datamart_instance_name}\" /Action:Install /Quiet:True /EnterpriseDbServer:#{@sqlserver_name} /EnterpriseDbName:#{@v1_db_name} /DatamartDbServer:#{@sqlserver_name} /DatamartDbName:#{@datamart_db_name} /AnalyticsDbServer:#{@sqlserver_name} /AnalyticsDbName:#{@analytics_db_name}"
 
 end
@@ -215,4 +206,19 @@ task :install_analytics do
 	setup_exe = (Dir[FileList["#{@installer_dir}/Setup-VersionOne.Analytics*.exe"].last]).to_s
 	print "Install #{@analytics_instance_name}\n"
 	sh "#{setup_exe} #{@analytics_instance_name} /Action:Install /Quiet /DatamartDbServer:#{@sqlserver_name} /DatamartDbName:#{@datamart_db_name} /AnalyticsDbServer:#{@sqlserver_name}  /AnalyticsDbName:#{@analytics_db_name} /EnterpriseUrl:#{@core_url} /SigningKey:versionone"
+end
+
+# #############################################################################
+# Utility 
+# #############################################################################
+def unzip_file (file)
+	destination = File.join File.dirname(__FILE__), @backup_file_directory
+	puts "Extract #{file} into #{destination}"
+	Zip::ZipFile.open(file) { |zip_file|
+		zip_file.each { |f|
+			f_path=File.join(destination, f.name)
+			FileUtils.mkdir_p(File.dirname(f_path))
+			zip_file.extract(f, f_path) unless File.exist?(f_path)
+		}
+	}
 end
